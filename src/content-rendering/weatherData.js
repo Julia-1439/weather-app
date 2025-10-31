e1b7b9643f35372c263067a0ed96227104830f95
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-10-31 14:51:19 -0700

    Add styling to the page

--- src/content-rendering/weatherData.js
+++ src/content-rendering/weatherData.js
@@ -28,13 +28,14 @@ async function renderDay(data) {
 
   const header = document.createElement('div')
   header.classList.add('header');
-  const date = document.createElement('span');
+  const date = document.createElement('p');
   date.classList.add('date');
   date.textContent = data.datetime + dateFormatter(data.datetime, ', EEEE');
 
   const middle = document.createElement('div');
   middle.classList.add('middle');
   const iconContainer = document.createElement('div');
+  iconContainer.classList.add('icon-container');
   const icon = document.createElement('img');
   icon.classList.add('icon');
   const {default: imgURL} = await import(`./assets/icons/${data.icon}.png`);
@@ -44,27 +45,31 @@ async function renderDay(data) {
   const tempMaxContainer = document.createElement('div');
   tempMaxContainer.classList.add('maxtemp')
   const tempMaxPara = document.createElement('p');
+  tempMaxPara.classList.add('stat-header');
   tempMaxPara.textContent = 'Max';
   const tempMax = document.createTextNode(data.tempmax + (data.tempunit === 'f' ? '°F' : '°C'));
   const tempMinContainer = document.createElement('div');
   tempMinContainer.classList.add('mintemp')
   const tempMinPara = document.createElement('p');
+  tempMinPara.classList.add('stat-header');
   tempMinPara.textContent = 'Min';
   const tempMin = document.createTextNode(data.tempmin + (data.tempunit === 'f' ? '°F' : '°C'));
   const precipContainer = document.createElement('div');
   precipContainer.classList.add('precip');
   const precipPara = document.createElement('p');
+  precipPara.classList.add('stat-header');
   precipPara.textContent = 'Rain';
   const precip = document.createTextNode(data.precip + ' in');
   const precipProbContainer = document.createElement('div');
   precipProbContainer.classList.add('precipprob');
   const precipProbPara = document.createElement('p');
+  precipProbPara.classList.add('stat-header');
   precipProbPara.textContent = 'Precip %';
   const precipProb = document.createTextNode(data.precipprob + ' %');
   
   const footer = document.createElement('div');
   footer.classList.add('footer');
-  const description = document.createElement('span');
+  const description = document.createElement('p');
   description.classList.add('description');
   description.textContent = data.description;
 
